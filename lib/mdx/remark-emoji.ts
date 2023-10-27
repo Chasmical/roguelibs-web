af0fb8e7110240ddb84c753cd4638f22b3bf366b
Abbysssal <hillankeyxl2@yandex.ru>
2023-10-27 18:18:50 +0700

    🐛 Fix twemoji parsing regex

--- lib/mdx/remark-emoji.ts
+++ lib/mdx/remark-emoji.ts
@@ -2,20 +2,18 @@ import type { Transformer } from "unified";
 import { findAndReplace } from "mdast-util-find-and-replace";
 import type { PhrasingContent } from "mdast";
 import { get as getEmoji } from "node-emoji";
-import twemoji from "twemoji";
+import { toCodePoints } from "twemoji-parser";
+import twemojiRegex from "twemoji-parser/dist/lib/regex"; // see ./twemoji-parser.d.ts
 import { gemoji } from "gemoji";
 
 // Code adapted and repurposed from the following plugins:
 // - https://github.com/rhysd/remark-emoji
-// - https://github.com/madiodio/remark-twemoji
+// - https://github.com/twitter/twemoji-parser
 // - https://github.com/florianeckerstorfer/remark-a11y-emoji
 
 const EmojiNameRegex = /:\+1:|:-1:|:[\w-]+:/g;
 
-const EmojiRegex = new RegExp(
-  "(\\\\*)([\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])",
-  "g",
-);
+const EmojiRegex = new RegExp(`(\\\\*)(${twemojiRegex.source})`, "g");
 
 export interface RemarkEmojiOptions {
   className?: string;
@@ -39,7 +37,7 @@ export default function remarkEmoji(options?: RemarkEmojiOptions): Transformer {
     });
     // replace emojis with twemojis
     findAndReplace(tree, EmojiRegex, (_, escapes: string, emoji: string) => {
-      const codepoint = twemoji.convert.toCodePoint(emoji);
+      const codepoint = toCodePoints(emoji).join("-");
       const src = base + size + codepoint + ext;
       const ariaLabel = getEmojiDescription(emoji);
 
