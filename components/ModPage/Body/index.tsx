8094d7579c5ed60a6200897a2d74c904f5dc8b3c
Abbysssal <hillankeyxl2@yandex.ru>
2023-12-02 01:26:27 +0700

    ✨ Add description and releases tabs to mod page

--- components/ModPage/Body/index.tsx
+++ components/ModPage/Body/index.tsx
@@ -17,19 +17,43 @@ export interface ModPageBodyProps extends ModPageContext {
 export default function ModPageBody(props: ModPageBodyProps) {
   return (
     <div className={styles.wrapper}>
-      <ModPageDescription {...props} />
+      <Tabs className={styles.bodyTabs} lazy faded>
+        <TabItem icon="copy" label="Description">
+          <ModPageDescription {...props} />
+        </TabItem>
+        <TabItem icon="copy" label="Releases">
+          <ModPageReleases {...props} />
+        </TabItem>
+        {props.mode === "edit" && (
+          <TabItem icon="copy" label="Banner">
+            <ModPageBanner {...props} />
+          </TabItem>
+        )}
+      </Tabs>
     </div>
   );
 }
 
 export function ModPageDescription(props: ModPageBodyProps) {
-  const { mod, original, rscDescription, mode } = props;
+  const { mod, original, mutateMod, rscDescription, mode } = props;
   const [rscSource] = useState(original.description);
 
   return (
     <>
       {mode === "edit" ? (
-        <ModPageEditControls {...props} />
+        <>
+          <label>{"Description"}</label>
+          <TextArea
+            className="markdown"
+            value={mod.description}
+            height="300px"
+            autoTrimEnd={false}
+            onChange={v => mutateMod(m => void (m.description = v))}
+            error={description => {
+              if (description.length > 4000) return `Exceeded length limit (${description.length}/4000).`;
+            }}
+          />
+        </>
       ) : mod.description === rscSource ? (
         <div className="markdown">{rscDescription}</div>
       ) : (
@@ -39,7 +63,11 @@ export function ModPageDescription(props: ModPageBodyProps) {
   );
 }
 
-export function ModPageEditControls({ mod, mutateMod }: ModPageBodyProps) {
+export function ModPageReleases(props: ModPageBodyProps) {
+  return null;
+}
+
+export function ModPageBanner({ mod, mutateMod }: ModPageBodyProps) {
   const [fileUrl, setFileUrl] = useState<string | null>(null);
 
   function onChange(value: string) {
@@ -59,49 +87,36 @@ export function ModPageEditControls({ mod, mutateMod }: ModPageBodyProps) {
   }, [fileUrl]);
 
   return (
-    <>
-      <div className={styles.editBanner}>
-        <div className={styles.bannerUrlInput}>
-          <label>{"Banner"}</label>
-          <Tabs style={{ height: 150 }}>
-            <TabItem label="External URL">
-              <TextInput
-                value={mod.banner_url}
-                placeholder={"/placeholder.png"}
-                onChange={onChange}
-                error={banner_url => {
-                  if (banner_url.length > 255) return `Exceeded length limit (${banner_url.length}/255).`;
-                }}
-              />
-            </TabItem>
-            <TabItem label="Upload file">
-              <input type="file" onChange={onUpload} />
-            </TabItem>
-          </Tabs>
-        </div>
-        <div className={styles.bannerAlignmentGrid}>
-          {[1, 2, 3, 4, 5, 6, 7].map(num => (
-            <Button
-              key={num}
-              className={clsx(mod.banner_layout === num && styles.active)}
-              onClick={() => mutateMod(m => void (m.banner_layout = num))}
-            >
-              <Icon type={("align" + num) as IconType} />
-            </Button>
-          ))}
-        </div>
+    <div className={styles.editBanner}>
+      <div className={styles.bannerUrlInput}>
+        <label>{"Banner"}</label>
+        <Tabs style={{ height: 150 }}>
+          <TabItem label="External URL">
+            <TextInput
+              value={mod.banner_url}
+              placeholder={"/placeholder.png"}
+              onChange={onChange}
+              error={banner_url => {
+                if (banner_url.length > 255) return `Exceeded length limit (${banner_url.length}/255).`;
+              }}
+            />
+          </TabItem>
+          <TabItem label="Upload file">
+            <input type="file" onChange={onUpload} />
+          </TabItem>
+        </Tabs>
       </div>
-      <label>{"Description"}</label>
-      <TextArea
-        className="markdown"
-        value={mod.description}
-        height="300px"
-        autoTrimEnd={false}
-        onChange={v => mutateMod(m => void (m.description = v))}
-        error={description => {
-          if (description.length > 4000) return `Exceeded length limit (${description.length}/4000).`;
-        }}
-      />
-    </>
+      <div className={styles.bannerAlignmentGrid}>
+        {[1, 2, 3, 4, 5, 6, 7].map(num => (
+          <Button
+            key={num}
+            className={clsx(mod.banner_layout === num && styles.active)}
+            onClick={() => mutateMod(m => void (m.banner_layout = num))}
+          >
+            <Icon type={("align" + num) as IconType} />
+          </Button>
+        ))}
+      </div>
+    </div>
   );
 }
