7107d1504d566455ae1ee25f0ab0b5bd9189f60c
Abbysssal <hillankeyxl2@yandex.ru>
2023-12-09 17:35:02 +0700

    🐛 Fix invalid user mention links

--- lib/mdx/plugins/remark-mentions.tsx
+++ lib/mdx/plugins/remark-mentions.tsx
@@ -1,18 +1,22 @@
 import type { Transformer } from "unified";
 import { findAndReplace } from "mdast-util-find-and-replace";
 
-export interface RemarkMentionsOptions {}
+export interface RemarkMentionsOptions {
+  buildUrl?: (username: string) => string;
+}
 
 const MentionRegex = /@([0-9a-zA-Z][-0-9a-zA-Z_]*)/g;
 
 export default function remarkMentions(options?: RemarkMentionsOptions): Transformer {
+  const buildUrl = options?.buildUrl ?? (username => "/user/" + username);
+
   return (tree: any) => {
     findAndReplace(tree, [
       MentionRegex,
       (match, username) => {
         return {
           type: "link",
-          url: `/users/` + username,
+          url: buildUrl(username),
           children: [{ type: "strong", children: [{ type: "text", value: match }] }],
         };
       },
