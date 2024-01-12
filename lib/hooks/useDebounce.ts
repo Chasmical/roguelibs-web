9a484f6af4dfdcdaffc1fee0228e36f6aa408674
Abbysssal <hillankeyxl2@yandex.ru>
2024-01-12 19:44:55 +0700

    🧑‍💻 Make useDebounce functions immutable

--- lib/hooks/useDebounce.ts
+++ lib/hooks/useDebounce.ts
@@ -7,20 +7,18 @@ export default function useDebounce<Args extends any[]>(
   wait: number,
 ): [waiting: boolean, start: (...args: Args) => void, reset: () => void] {
   const callbackRef = useLatest(callback);
+  const waitRef = useLatest(wait);
   const [waiting, setWaiting] = useState(false);
   const timeoutRef = useRef<NodeJS.Timeout>();
 
-  const start = useCallback(
-    (...args: Args) => {
-      timeoutRef.current ? clearTimeout(timeoutRef.current) : setWaiting(true);
-      timeoutRef.current = setTimeout(() => {
-        timeoutRef.current = undefined;
-        setWaiting(false);
-        callbackRef.current(...args);
-      }, wait);
-    },
-    [wait],
-  );
+  const start = useCallback((...args: Args) => {
+    timeoutRef.current ? clearTimeout(timeoutRef.current) : setWaiting(true);
+    timeoutRef.current = setTimeout(() => {
+      timeoutRef.current = undefined;
+      setWaiting(false);
+      callbackRef.current(...args);
+    }, waitRef.current);
+  }, []);
 
   const reset = useCallback(() => {
     if (timeoutRef.current) {
